64a07eca5f7dbe3137192eed08c0c745e405da27
Camila Fernandes Trentino <124791512+Camilatrentino@users.noreply.github.com>
2024-03-28 08:29:27 -0300

    Add files via upload

---
+++ SW-main/SW-main/Exemplo/lampada/lampada/Lampada.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lampada
+{
+    public class Lampada
+    {
+        private bool ligada { get; set; }
+        private string? Cor { get; set; }
+        private int Potencia { get; set; }
+
+        public void ligar()
+        {
+
+            ligada = true;
+
+        }
+
+        public void Desligar()
+        {
+
+            ligada = false;
+
+        }
+
+        public bool estaLigada()
+        {
+
+            return ligada;
+
+        }
+
+        //COR
+
+        public void Cores()
+        {
+            Cor = "red";
+        }
+
+        public string qualCor()
+        {
+
+            return Cor;
+
+        }
+
+        //Potencia
+
+        public void Pot()
+        {
+
+            Potencia = 120;
+
+        }
+
+        public int qualPot()
+        {
+
+            return Potencia;
+
+        }
+
+
+    }
+}
---
+++ SW-main/SW-main/Exemplo/lampada/lampada/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace lampada
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //INSTANCIA DE UM OBJETO
+            Lampada lampada1 = new Lampada();
+            
+
+            lampada1.ligar();
+            lampada1.Cores();
+            lampada1.Pot();
+
+            Console.WriteLine("A lampada esta ligada?  " + lampada1.estaLigada());
+            Console.WriteLine("Qual a cor da lampadaa  " + lampada1.qualCor());
+            Console.WriteLine("Qual a potencia  " + lampada1.qualPot());
+
+
+
+
+        }
+    }
+}
